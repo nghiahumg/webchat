f2e3be687151056c03fe97c1407152052361cad0
nghia <nghia>
2024-04-19 16:43:22 +0700

    nghia

---
+++ service/config.php
@@ -0,0 +1,19 @@
+<?php
+// 这里填写客服系统域名，前面带http://，用于pusher系统通知客服平台客户或者客服上下线
+$domain = 'http://woline.test';
+
+// App_key，客服系统与pusher通讯的key
+$app_key = '6evty4dlicvh3ns4';
+
+// App_secret，客服系统与pusher通讯的密钥
+$app_secret = 'vvl9yf4267dapuwt04ujya3a7wndusgf';
+
+// App id
+$app_id = 232;
+
+// websocket 端口，客服系统网页会连这个端口
+$websocket_port = 9090;
+
+// Api 端口，用于后端与pusher通讯
+$api_port = 2080;
+
